a5b50bc5f7749aee55523b4e296fdd7fc2af8d9e
victorkwak <victorkwak@gmail.com>
2015-10-11 19:52:19 -0700

    Aliens!!!

--- CPPFoodRestaurant/app/controllers/Application.java
+++ CPPFoodRestaurant/app/controllers/Application.java
@@ -1,18 +1,20 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
     private static final org.slf4j.Logger logger = Logger.of(Application.class).underlying();
+
+    public Result aliens() {
+        logger.trace("Aliens! OoOoOoOoOoOo!!!");
+        return ok(index.render("Aliens! OoOoOoOoOoOo!!!"));
+    }
 
     public Result index() {
         logger.trace("index method called");
         return ok(index.render("Your new application is ready."));
     }
-
-    
-
 }
