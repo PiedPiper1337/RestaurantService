e5b277a926da67898c48b1aa7fb632558dd0221b
Brian Zhao <bzspartan@gmail.com>
2015-10-11 18:26:21 -0700

    added a method to test returning json strings

--- CPPFoodRestaurant/app/controllers/Application.java
+++ CPPFoodRestaurant/app/controllers/Application.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.HashMap;
 
 public class Application extends Controller {
     private static final org.slf4j.Logger logger = Logger.of(Application.class).underlying();
@@ -11,6 +15,21 @@ public class Application extends Controller {
     public Result index() {
         logger.trace("index method called");
         return ok(index.render("Your new application is ready."));
+    }
+
+
+    /**
+     * example json return method
+     * @return
+     */
+    public Result helloWorld() {
+        logger.trace("hello world method called");
+        HashMap javaObject = new HashMap();
+        javaObject.put("Message", "Hello World");
+        javaObject.put("Here is the key", "Here is the value");
+        JsonNode json = Json.toJson(javaObject);
+        logger.debug("message that is sent back by the server: {}", json.toString());
+        return ok(json.toString());
     }
 
     
