f8fb8856b3b495574d58a46e1e17251675d28db0
Brian Zhao <bzspartan@gmail.com>
2015-10-20 16:47:27 -0700

    added buildpackage.sh

---
+++ VideoSummary/BuildPackage.sh
@@ -0,0 +1,8 @@
+echo "===============Cleaning================"
+activator clean
+echo "===============Reloading==============="
+activator reload
+echo "===============Creating Zip============"
+activator dist
+echo "===============Copying to AWS============"
+scp -i deployAmazon target/universal/videosummary-1.0-SNAPSHOT.zip deploy@aws:~
