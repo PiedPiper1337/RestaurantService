9cfa25c5ebc14c2dd03e204ca951d8e8e7c8725a
Brian Zhao <bzspartan@gmail.com>
2015-10-25 14:07:31 -0700

    working!

--- VideoSummary/app/controllers/Application.java
+++ VideoSummary/app/controllers/Application.java
@@ -1,16 +1,15 @@
 package controllers;
 
-import org.openqa.selenium.WebDriver;
-import utils.Constants;
-import utils.StringManip;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.openqa.selenium.WebDriver;
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
-import utils.TranscriptGenerator;
+import utils.Constants;
+import utils.StringManip;
 import views.html.video;
 
 import javax.inject.Inject;
@@ -20,17 +19,22 @@ public class Application extends Controller {
     private static final org.slf4j.Logger logger = Logger.of(Application.class).underlying();
 
     @Inject
-    private WebDriver webDriver;
+    @Singleton
+    private static WebDriver webDriver;
 
 
     @With(IPAction.class)
-
     public Result index() {
         logger.trace("index method called");
-//        return ok(views.html.index.render());
-//        TranscriptGenerator.getTranscriptFromFullURL(null);
-        return ok(webDriver.toString());
+        return ok(views.html.index.render());
     }
+
+
+    public Result genericFailure(String badResource) {
+        logger.trace("bad url attempt at {}", badResource);
+        return badRequest("This page does not exist.");
+    }
+
 
     /**
      * takes a url with v parameter
@@ -64,30 +68,5 @@ public class Application extends Controller {
     }
 
 
-//
-//    /**
-//     * example json return method
-//     *
-//     * @return
-//     */
-//    public Result helloWorld() {
-//        logger.trace("hello world method called");
-//        HashMap<String,String> javaObject = new HashMap<>();
-//        javaObject.put("Message", "Hello World");
-//        javaObject.put("Here is the key", "Here is the value");
-//        JsonNode json = Json.toJson(javaObject);
-//        logger.debug("message that is sent back by the server: {}", json.toString());
-//        return ok(json.toString());
-//    }
-
-//    public Result login() {
-//        logger.trace("login method called");
-//        return ok(index.render("This is the login page"));
-//    }
-
-//    public Result aliens() {
-//        logger.trace("Aliens! OoOoOoOoOoOo!!!");
-//        return ok(index.render("Aliens! OoOoOoOoOoOo!!!"));
-//    }
 
 }
--- VideoSummary/app/modules/InitializerModule.java
+++ VideoSummary/app/modules/InitializerModule.java
@@ -1,24 +1,41 @@
 package modules;
 
+import com.google.inject.AbstractModule;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import play.api.Configuration;
-import play.api.Environment;
-import play.api.inject.Binding;
-import play.api.inject.Module;
-
-import scala.collection.Seq;
-
-import javax.inject.Singleton;
+import play.Logger;
 
 /**
  * Created by brianzhao on 10/25/15.
  */
-public class InitializerModule extends Module {
-    @Override
-    public Seq<Binding<?>> bindings(Environment environment, Configuration configuration) {
-        return seq(
-                bind(WebDriver.class).to(ChromeDriver.class));
+public class InitializerModule extends AbstractModule {
+    private static final org.slf4j.Logger logger = Logger.of(InitializerModule.class).underlying();
+
+    enum OSType {
+        Linux, Mac, Windows
     }
 
+    @Override
+    protected void configure() {
+        OSType currentOS;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.startsWith("mac")) {
+            currentOS = OSType.Mac;
+        } else if (osName.startsWith("window")) {
+            currentOS = OSType.Windows;
+        } else {
+            currentOS = OSType.Linux;
+        }
+        logger.debug("Detected operating system: {}", currentOS);
+
+        if (currentOS.equals(OSType.Mac)) {
+            System.setProperty("webdriver.chrome.driver", "chromedriverMac");
+        } else if (currentOS.equals(OSType.Linux)) {
+            System.setProperty("webdriver.chrome.driver", "chromedriverLinux");
+        }
+        logger.debug("chromedriver environment path set");
+        bind(WebDriver.class).to(ChromeDriver.class).asEagerSingleton();
+    }
 }
+
+
