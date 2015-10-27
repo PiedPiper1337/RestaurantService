fd5f458412e610a4698ff941f1b3253ad709eac6
victorkwak <victorkwak@gmail.com>
2015-10-27 11:49:12 -0700

    Stuff

---
+++ VideoSummary/app/utils/Factories/PhantomFactory.java
@@ -0,0 +1,28 @@
+package utils.Factories;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by brianzhao on 10/25/15.
+ */
+public class PhantomFactory {
+    private static PhantomJSDriver phantomJsDriver;
+    public static PhantomJSDriver getInstance() {
+        if (phantomJsDriver == null) {
+            Capabilities caps = new DesiredCapabilities();
+            ((DesiredCapabilities) caps).setJavascriptEnabled(true);
+            ((DesiredCapabilities) caps).setCapability(
+                    PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                    "phantomjs"
+            );
+            phantomJsDriver = new PhantomJSDriver(caps);
+            return phantomJsDriver;
+        } else {
+            return phantomJsDriver;
+        }
+    }
+
+}
