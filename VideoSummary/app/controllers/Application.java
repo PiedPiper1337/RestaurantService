43c0ff0a2ea02f07950394b229c3c21b8570b724
Brian Zhao <bzspartan@gmail.com>
2015-11-04 10:13:30 -0800

    still need to fix post endpoint

--- VideoSummary/app/controllers/Application.java
+++ VideoSummary/app/controllers/Application.java
@@ -1,20 +1,24 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 import play.Logger;
+import play.cache.CacheApi;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
-import utils.Pipeline;
-import utils.StringManip;
+import utils.*;
 import utils.Summarizer.Group;
 import utils.Summarizer.Summary;
-import utils.TranscriptGenerator;
 import views.html.video;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 //import utils.Summarizer.Summary;
 
@@ -25,6 +29,8 @@ import java.util.ArrayList;
 public class Application extends Controller {
     private static final org.slf4j.Logger logger = Logger.of(Application.class).underlying();
 
+    @Inject
+    CacheApi cache;
 
     @With(IPAction.class)
     public Result index() {
@@ -45,6 +51,23 @@ public class Application extends Controller {
 
     public Result blank() {
         return ok();
+    }
+
+    public Result summaryTimes() {
+        JsonNode requestBodyParams = request().body().asJson();
+        JsonNode videoIdNode = requestBodyParams.get("v");
+        String videoId = videoIdNode.asText();
+
+        String result = cache.get(videoId);
+        if (result == null) {
+            ArrayList<Group> summaryGroups = SummaryGenerator.generate(videoId);
+            if (summaryGroups == null) {
+                return internalServerError("Sorry but we had an error trying to process your video");
+            } else {
+                result = Json.toJson(summaryGroups).toString();
+            }
+        }
+        return ok(result);
     }
 
 
@@ -105,19 +128,18 @@ public class Application extends Controller {
 
     public Result summarize() {
         logger.trace("Summarization");
-        String videoId = request().getQueryString("s");
+        String videoId = request().getQueryString("v");
         if (videoId == null) {
             logger.debug("summarization parameter query was null, redirecting to index");
             return redirect("/");
         }
-        String transcript = TranscriptGenerator.getTranscript(videoId);
-        if (transcript == null) {
+
+        ArrayList<Group> summaryGroups = SummaryGenerator.generate(videoId);
+        if (summaryGroups== null) {
             return internalServerError("Sorry but we had an error processing your video");
         }
-
-        Summary summary = new Summary(transcript);
-//        ArrayList<Group> summaryGroups = summary.generateSummary(null, null, null, null, null);
-        ArrayList<Group> summaryGroups = summary.generateSummary();
+        cache.set(videoId, Json.toJson(summaryGroups).toString(), Constants.CACHE_TIME);
         return ok(summaryGroups.toString());
     }
+
 }
--- VideoSummary/app/utils/StringManip.java
+++ VideoSummary/app/utils/StringManip.java
@@ -1,31 +1,36 @@
 package utils;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.protocol.HTTP;
+import play.Logger;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by brianzhao on 10/13/15.
  */
 public class StringManip {
+    private static final org.slf4j.Logger logger = Logger.of(StringManip.class).underlying();
     public static String extractParameter(String url, String key) {
-        String[] entireUrlString = url.split("\\?");
-        if (entireUrlString.length != 2) {
-            throw new RuntimeException();
+        try {
+            List<NameValuePair> results = URLEncodedUtils.parse(new URL(url).toURI(), HTTP.UTF_8);
+            for (NameValuePair nameValuePair : results) {
+                if (nameValuePair.getName().equalsIgnoreCase(key)) {
+                    logger.debug(nameValuePair.getValue());
+                    return nameValuePair.getValue();
+                }
+            }
+            logger.error("key: {} in url: {} not found", key, url);
+            return null;
+        } catch (Exception e) {
+            logger.error("exception with url {}" , url);
+            e.printStackTrace();
+            return null;
         }
-        String params = entireUrlString[1];
-        String[] keyValue = params.split("=");
-        if (keyValue.length % 2 != 0) {
-            throw new RuntimeException();
-        }
-        HashMap<String, String> keyValueMap = new HashMap<>();
-
-        for (int i = 0; i < keyValue.length; i += 2) {
-            keyValueMap.put(keyValue[i], keyValue[i + 1]);
-        }
-        String toReturn = keyValueMap.get(key);
-        if (toReturn == null) {
-            throw new RuntimeException();
-        }
-        return toReturn;
     }
 
     /**
