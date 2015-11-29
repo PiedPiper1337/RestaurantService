c7ffdad9da94b8f7b8bb9d83d3ba4da68913e124
Cary <caryando@gmail.com>
2015-11-28 22:31:14 -0800

    Refactor histogram to scatter plot

--- VideoSummary/public/javascripts/hello.js
+++ VideoSummary/public/javascripts/hello.js
@@ -193,94 +193,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 histogramX.push(histogram[i].startTimeSeconds);
             }
 
-            var x = [];
-            for (var i = 0; i < 500; i ++) {
-                x[i] = Math.random();
-            }
-
             var data = [
                 {
-                    autobiny: false,
-                    ybins: {
-                        start: 1,
-                        end: 70,
-                        size: 1
-                    },
-
                     x: histogramX,
                     y: histogramY,
-                    type: 'histogram'
+                    mode: 'lines+markers',
+                    type: 'scatter'
                 }
             ];
 
             var layout = {
-                bargap: .25,
                 title: "Importance vs. Time",
                 xaxis: {title: "Seconds"},
                 yaxis: {title: "Importance"}
             };
 
             Plotly.newPlot('histogram-div', data, layout);
-
-            /*
-            * Add the histogram
-            *
-            // Generate a Bates distribution of 10 random variables.
-            var values = d3.range(100).map(d3.random.bates(10));
-
-            // A formatter for counts.
-            var formatCount = d3.format(",.0f");
-
-            var margin = {top: 20, right: 20, bottom: 35, left: 20},
-                width = 500 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
-
-            var x = d3.scale.linear()
-                .domain([histogram[0].startTimeSeconds, histogram[histogram.length - 1].startTimeSeconds])
-                .range([0, width]);
-
-            // Generate a histogram using twenty uniformly-spaced bins.
-            var data = d3.layout.histogram()
-                .bins(x.ticks(100))
-                (histogram);
-
-            var y = d3.scale.linear()
-                .domain([0, d3.max(data, function(d) { return d.y; })])
-                .range([height, 0]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom");
-
-            var svg = d3.select("#histogram-div").append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            var bar = svg.selectAll(".bar")
-                .data(data)
-                .enter().append("g")
-                .attr("class", "bar")
-                .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-
-            bar.append("rect")
-                .attr("x", 1)
-                .attr("width", x(data[0].dx) - 1)
-                .attr("height", function(d) { return height - y(d.y); });
-
-            bar.append("text")
-                .attr("dy", ".75em")
-                .attr("y", 6)
-                .attr("x", x(data[0].dx) / 2)
-                .attr("text-anchor", "middle")
-                .text(function(d) { return formatCount(d.y); });
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-                */
         });
 
         apiRequest.fail(function(data) {
