97c06f4d7669e282048495e0ec57c53e0a79d859
ChoiByungWook <dchoi1@cpp.edu>
2016-05-29 22:58:26 -0700

    implemented bootstraps column to main page, added refs to updated time slices

--- VideoSummary/public/javascripts/hello.js
+++ VideoSummary/public/javascripts/hello.js
@@ -117,10 +117,10 @@ document.addEventListener("DOMContentLoaded", function () {
             if (slices.length > 0) {
                 $("#playlist-div").empty(); // Clear the playlist
                 for (var i = 0; i < slices.length; i++) {
-                    $("#playlist-div").append('<div class="section">' + (i+1) + '.) ' +
+                    $("#playlist-div").append('<a href=""><div class="section">' + (i+1) + '.) ' +
                         slices[i].startTime + ' - ' + slices[i].endTime + '<br/>' +
                         slices[i].wordsSpoken.substring(0, 50) +
-                        '...</div>');
+                        '...</div></a>');
                 }
 
                 highlightPlaylistIndex(gSliceIndex); // Highlight the current playlist index
