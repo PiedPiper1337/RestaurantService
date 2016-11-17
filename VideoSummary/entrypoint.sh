5a55831f394932903a70002996c27f7cd20bb353
brianzhao <bzspartan@gmail.com>
2016-11-16 18:48:38 -0800

    removed xvfb

--- VideoSummary/entrypoint.sh
+++ VideoSummary/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #note that this is only a backup copy of the file used to deploy on the server
 service mysql start
-Xvfb :10 -ac &
 cd /root/videosummary-1.0-SNAPSHOT/bin
 ./videosummary -Dhttp.port=80 -Dplay.evolutions.db.default.autoApply=true -Dplay.evolutions.db.default.autoApplyDowns=true
